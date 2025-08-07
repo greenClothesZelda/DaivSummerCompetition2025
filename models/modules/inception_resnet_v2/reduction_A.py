e83aa9673abb6587c883a9d31b57cd9efab2a1cd
김진수 <wlstn3697@naver.com>
2025-08-07 17:11:34 +0900

    complete to making modules

--- models/modules/inception_resnet_v2/reduction_A.py
+++ models/modules/inception_resnet_v2/reduction_A.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 from models.modules.conv_block import ConvBlock
 
 class ReductionA(nn.Module):
-    def __init__(self, k, l, m, n):
+    def __init__(self, k, l, m, n, in_channels=384):
         super().__init__()
-        # input 35x35x384, output 17x17x1024
+        # input 35x35xin_channels, output 17x17x1024
         self.k = k
         self.l = l
         self.m = m
@@ -18,12 +18,12 @@ class ReductionA(nn.Module):
 
         # input 35x35x384, output 17x17xn
         self.branch2 = nn.Sequential(
-            ConvBlock(in_channels=384, out_channels=n, kernel_size=3, stride=2, padding=0), # output: 17x17xn
+            ConvBlock(in_channels=in_channels, out_channels=n, kernel_size=3, stride=2, padding=0), # output: 17x17xn
         )
 
         # input 35x35x384, output 17x17xm
         self.branch3 = nn.Sequential(
-            ConvBlock(in_channels=384, out_channels=k, kernel_size=1, stride=1, padding=0), # output: 35x35xk
+            ConvBlock(in_channels=in_channels, out_channels=k, kernel_size=1, stride=1, padding=0), # output: 35x35xk
             ConvBlock(in_channels=k, out_channels=l, kernel_size=(3, 3), stride=1, padding=(1, 1)), # output: 35x35xl
             ConvBlock(in_channels=l, out_channels=m, kernel_size=(3, 3), stride=2, padding=0), # output: 17x17xm
         )
@@ -39,7 +39,8 @@ class ReductionA(nn.Module):
         return x
 
 if __name__ == "__main__":
-    reduction_a = ReductionA(k=192, l=224, m=256, n=384)
-    x = torch.randn(1, 384, 35, 35)  # Batch size of 1, 384 channels, 35x35 feature map
+    in_channel = 384
+    reduction_a = ReductionA(k=256, l=256, m=384, n=384, in_channels=in_channel)
+    x = torch.randn(1, in_channel, 35, 35)  # Batch size of 1, 384 channels, 35x35 feature map
     output = reduction_a(x)
-    print(output.shape)  # Should print the shape of the output tensor
+    print(output.shape)
