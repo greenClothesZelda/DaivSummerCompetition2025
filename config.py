91a2f14c1e13cb1f902f41c9b930b804e7542d66
김진수 <100560249+greenClothesZelda@users.noreply.github.com>
2025-08-12 09:57:37 +0900

    fix loader

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ BATCH_SIZE = 128
 LEARNING_RATE = 0.001
 EPOCHS = 3
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DATA_PATH = "C:/Users/wlstn/.cache/kagglehub/datasets"
--- data/loader.py
+++ data/loader.py
@@ -1,53 +1,45 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+from pathlib import Path
+import config
 
-def get_cifar100_loaders(batch_size:int, num_workers:int, root="./data")->tuple[DataLoader, DataLoader]:
-    # Define the transformations for the CIFAR-100 dataset
-    mean = (0.5071, 0.4867, 0.4408)
-    std = (0.2675, 0.2565, 0.2761)
+from data.competition_data import ImageDataset
 
-    transform_train = transforms.Compose([
-        # image의 원래 크기는 32x32이지만 padding을 추가하여 랜덤 크롭을 적용 =>이미지 크기는 동일 그러나 증강됨
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),  # 랜덤으로 좌우 반전
-        transforms.ToTensor(),  # 텐서로 변환
-        transforms.Normalize(mean, std)  # 정규화
-    ])
+# Image Resizing and Tensor Conversion
+IMG_SIZE = (64, 64)
 
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),  # 텐서로 변환
-        transforms.Normalize(mean, std)  # 정규화
-    ])
+# ImageNet Normalization
+IMG_NORM = dict(
+    mean=[0.485, 0.456, 0.406],
+    std=[0.229, 0.224, 0.225]
+)
 
-    train_set = datasets.CIFAR100(
-        root=root,
-        train=True,
-        download=True,
-        #transform에 위에서 정의한 증강 방식을 적용
-        transform=transform_train
-    )
+resizer = transforms.Compose([
+    transforms.Resize(IMG_SIZE), # Resize Image
+    transforms.ToTensor(), # Convert Image to Tensor
+    transforms.Normalize(**IMG_NORM) # Normalization
+])
 
-    test_set = datasets.CIFAR100(
-        root=root,
-        train=False,
-        download=True,
-        transform=transform_test
-    )
+valid_transform = transforms.Compose([
+    transforms.Resize(IMG_SIZE),
+    transforms.ToTensor(),
+    transforms.Normalize(**IMG_NORM)
+])
 
-    train_loader = DataLoader(
-        train_set,
-        batch_size=batch_size,
-        shuffle=True,
-        #IOBound를 처리하기 위함
-        num_workers=num_workers,
-        pin_memory=True
-    )
+path = Path(config.DATA_PATH)
 
-    test_loader = DataLoader(
-        test_set,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=num_workers,
-        pin_memory=True
-    )
-    return train_loader, test_loader
+print(f"Dataset Path: {path}")
+print(path.is_absolute())
+
+
+train_dataset = ImageDataset(root=path, force_download=False, train=True, transform=resizer)
+valid_dataset = ImageDataset(root=path, force_download=False, valid=True, transform=valid_transform)
+test_dataset = ImageDataset(root=path, force_download=False, train=False, transform=resizer)
+unlabeled_dataset = ImageDataset(root=path, force_download=False, unlabeled=True, transform=resizer)
+
+
+if __name__ == "__main__":
+    print(f"Train Dataset Size: {len(train_dataset)}")
+    print(f"Valid Dataset Size: {len(valid_dataset)}")
+    print(f"Test Dataset Size: {len(test_dataset)}")
+    print(f"Unlabeled Dataset Size: {len(unlabeled_dataset)}")
